62046481841cba5e8c08246bb44e7ba5469d3690
jdc <jdcxxx@gmail.com>
2017-02-17 07:29:55 -0800

    add phpunit tests for each spec

--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -1,4 +1,40 @@
 <?php
+    require_once "src/RepeatCounter.php";
+    class RepeatCounterTest extends PHPUnit_Framework_TestCase
+    {
+        function test_match_word()
+        {
+            $findWord = 'foo';
+            $stringToSearch = "Foo!";
+            $newCounter = new RepeatCounter;
 
+            $result = RepeatCounter-CountRepeats($findWord, $stringToSearch);
+
+            $this->assertEquals(1, $results);
+        }
+        //
+        // function test_match_in_sentence()
+        // {
+        //     $findWord = 'bar';
+        //     $stringToSearch = "The word is BaR.";
+        //     $newCounter = new RepeatCounter;
+        //
+        //     $result = RepeatCounter-CountRepeats($findWord, $stringToSearch);
+        //
+        //     $this->assertEquals(1, $results);
+        // }
+        //
+        // function test_match_multiple_in_sentence()
+        // {
+        //     $findWord = 'foo';
+        //     $stringToSearch = "The word is FOO. It is common for the FOO the live in a cave near the FOO mountains.";
+        //     $newCounter = new RepeatCounter;
+        //
+        //     $result = RepeatCounter-CountRepeats($findWord, $stringToSearch);
+        //
+        //     $this->assertEquals(3, $results);
+        // }
+
+    }
 
  ?>
