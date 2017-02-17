4052ea36f9b99513ec1c77565c5252f67d1b042d
jdc <jdcxxx@gmail.com>
2017-02-17 10:38:50 -0800

    add twig templates to link in app.php

---
+++ app/app.php
@@ -0,0 +1,29 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/RepeatCounter.php";
+
+    $app = new Silex\Application();
+
+    $app['debug'] = true;
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('word_finder.html.twig');
+    });
+
+    $app->post("/finder", function() use ($app) {
+        $newCounter = new RepeatCounter;
+        $searchWord = $_POST['word-to-find'];
+        $searchText = $_POST['search-string'];
+        $numberOfFinds = $newCounter->CountRepeats($searchWord, $searchText);
+        return $app['twig']->render('word_finder_results.html.twig', array('word'=>$searchWord, 'text' => $searchText, 'count' => $numberOfFinds));
+    });
+
+
+    return $app;
+ ?>
